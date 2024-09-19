7b8d2904cf2abd67bd094a98b7acb2767b1c2591
mitrasingh <mitra.coder@gmail.com>
2024-09-18 21:53:43 -0400

    feat: add loginGroup instance of FormGroup, add email and password properties as FormControl

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  onSubmit(): void {
+    console.log(
+      `email: ${this.loginForm.value.email}, password: ${this.loginForm.value.password}`
+    );
+  }
 }
