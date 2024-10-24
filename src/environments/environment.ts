8f5f3a06260758e979cf1a462aee77b2395423a0
mitrasingh <mitra.coder@gmail.com>
2024-10-24 19:23:14 -0400

    refactor: update environment object to include production and firebase property

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCEqfjMbQrmkdMrKYpDHRld8v7-ePiVAg8',
-  authDomain: 'workflow-app-40066.firebaseapp.com',
-  projectId: 'workflow-app-40066',
-  storageBucket: 'workflow-app-40066.appspot.com',
-  messagingSenderId: '998818644978',
-  appId: '1:998818644978:web:65f9c834f11317d967a16b',
-  measurementId: 'G-0W167WX3N0',
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCEqfjMbQrmkdMrKYpDHRld8v7-ePiVAg8',
+    authDomain: 'workflow-app-40066.firebaseapp.com',
+    projectId: 'workflow-app-40066',
+    storageBucket: 'workflow-app-40066.appspot.com',
+    messagingSenderId: '998818644978',
+    appId: '1:998818644978:web:65f9c834f11317d967a16b',
+    measurementId: 'G-0W167WX3N0',
+  },
 };
