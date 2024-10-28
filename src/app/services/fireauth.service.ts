eb536a8868e8bd7941e3bb271d4bb3788f98b215
mitrasingh <mitra.coder@gmail.com>
2024-10-27 22:44:53 -0400

    feat: create private property for authentication status of user, create a constructor for instance of Auth service from Firebase

--- src/app/services/fireauth.service.ts
+++ src/app/services/fireauth.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Auth, User } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FireAuthService {}
+export class FireAuthService {
+  private user: User | null = null;
+  constructor(private auth: Auth) {}
+}
+
+// Listen to auth state changes
+
+// Sign in
+
+// Sign up user
+
+// Sign Out
