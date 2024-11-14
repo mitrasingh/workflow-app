9265e4eb089486802735569c1b1346148fe00429
mitrasingh <mitra.coder@gmail.com>
2024-11-14 15:13:30 -0500

    feat: add hero component to imports

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
+import { HeroComponent } from '../shared/components/hero/hero.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, SignupComponent],
+  imports: [ReactiveFormsModule, RouterLink, SignupComponent, HeroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
