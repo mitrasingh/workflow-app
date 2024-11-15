22a8ea313e9c8635a2318d9abcad1eb12b0ad4e6
mitrasingh <mitra.coder@gmail.com>
2024-11-14 19:07:31 -0500

    feat: import login form component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,11 +8,19 @@ import {
 import { RouterLink } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
 import { HeroComponent } from '../shared/components/hero/hero.component';
+import { LoginForm } from './ui/login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, SignupComponent, HeroComponent],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    SignupComponent,
+    HeroComponent,
+    LoginComponent,
+    LoginForm,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
