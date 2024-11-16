49621b9f22804fd4bc5cc7e66901bfcaa72a0c2c
mitrasingh <mitra.coder@gmail.com>
2024-11-16 16:14:42 -0500

    chore: remove login form code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,30 +25,5 @@ import { LoginForm } from './ui/login-form/login-form.component';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  submitted = false;
-  loginForm = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.email],
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required],
-    }),
-  });
-
-  onSubmit(): void {
-    this.submitted = true;
-
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    console.log(this.loginForm.value);
-    this.loginForm.reset();
-    this.submitted = false;
-  }
-
-  // Getter to access form control easier and more readable in the template
-  get email() {
-    return this.loginForm.get('email');
-  }
+  onSubmit(): void {}
 }
