1748c6bc611e79913d140d885bd1c13bc91dd823
mitrasingh <mitra.coder@gmail.com>
2024-11-20 18:21:50 -0500

    refactor: remove code no longer in use, add signupUser method, comment out code which will be used laterg

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,62 +4,37 @@ import { RouterLink } from '@angular/router';
 import { passwordMatchValidator } from '../shared/utils/passwordMatchValidator';
 import { FireAuthService } from '../services/fireauth.service';
 import { HeroComponent } from '../shared/components/hero/hero.component';
+import { SignupForm } from './ui/signup-form/signup-form.component';
+import { Signup } from '../models/signup.model';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, HeroComponent],
+  imports: [ReactiveFormsModule, RouterLink, HeroComponent, SignupForm],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  private authService = inject(FireAuthService);
-  fb = new FormBuilder();
-  submitted = false;
-  signupForm = this.fb.nonNullable.group(
-    {
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-    },
-    {
-      validators: [passwordMatchValidator],
-    }
-  );
+  // private authService = inject(FireAuthService);
 
-  // Getter to access form control easier and more readable in the template
-  get firstName() {
-    return this.signupForm.get('firstName');
-  }
-
-  get lastName() {
-    return this.signupForm.get('lastName');
-  }
-
-  get email() {
-    return this.signupForm.get('email');
-  }
-
-  get password() {
-    return this.signupForm.get('password');
+  signupUser(signupData: Signup) {
+    console.log(signupData);
   }
 
   onSubmit(): void {
-    this.submitted = true;
-    const formData = this.signupForm.getRawValue();
-    if (this.signupForm.valid) {
-      console.log(formData.firstName);
-      this.authService.register(
-        formData.firstName,
-        formData.lastName,
-        formData.email,
-        formData.password
-      );
-      console.log('Submitted!');
-      this.signupForm.reset();
-      this.submitted = false;
-    }
+    // this.submitted = true;
+    // const formData = this.signupForm.getRawValue();
+    // if (this.signupForm.valid) {
+    //   console.log(formData.firstName);
+    //   this.authService.register(
+    //     formData.firstName,
+    //     formData.lastName,
+    //     formData.email,
+    //     formData.password
+    //   );
+    //   console.log('Submitted!');
+    //   this.signupForm.reset();
+    //   this.submitted = false;
+    // }
   }
 }
