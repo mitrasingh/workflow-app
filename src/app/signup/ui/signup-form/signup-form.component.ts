26e02a29fcd2bbba15a0a778f35ca833e4072436
mitrasingh <mitra.coder@gmail.com>
2024-11-20 14:33:07 -0500

    feat: add output of input values, add form builder, add validators, add getters

--- src/app/signup/ui/signup-form/signup-form.component.ts
+++ src/app/signup/ui/signup-form/signup-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { Signup } from '../../../models/signup.model';
+import { FormBuilder, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../../../shared/utils/passwordMatchValidator';
 
 @Component({
   selector: 'app-signup-form',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.css',
 })
-export class SignupForm {}
+export class SignupForm {
+  formData = output<Signup>();
+  fb = new FormBuilder();
+
+  signupForm = this.fb.nonNullable.group(
+    {
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: passwordMatchValidator }
+  );
+
+  // Getter to access form control easier and more readable in the template
+  get firstName() {
+    return this.signupForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.signupForm.get('lastName');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+}
